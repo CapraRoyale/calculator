c26a5c663181e22783185e3289e25b08db92aaa0
Siege Lehman <siege.lehman@gmail.com>
2020-06-22 08:59:13 -0700

    Updates

---
+++ precache-manifest.35d19ed47e44980d623aeb03121b9afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9bd1e8d8789448f53df9f2d3aca6092",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "3f3d2bb1195b9d28b67c",
+    "url": "/calculator/static/css/2.5861e1ce.chunk.css"
+  },
+  {
+    "revision": "c5af004dd915c78149d4",
+    "url": "/calculator/static/css/main.86f680a8.chunk.css"
+  },
+  {
+    "revision": "3f3d2bb1195b9d28b67c",
+    "url": "/calculator/static/js/2.92270733.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/calculator/static/js/2.92270733.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5af004dd915c78149d4",
+    "url": "/calculator/static/js/main.5f5d6851.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
